8da4f0c35aaa6705ff43b100dae3c6e718c4cec9
Caspar Green <caspar@icasparwebdevelopment.com>
2014-05-28 15:09:44 -0400

    Add new post-types and taxonomy placeholder admin option pages

---
+++ library/admin/templates/arras-posttypes.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * arras-posttypes.php
+ *
+ * Displays settings for Arras post types options
+ *
+ * @author Caspar Green <caspar@iCasparWebDevelopment.com>
+ *
+ * @package Arras
+ * @since 1.6
+ * 
+ */
+
---
+++ library/admin/templates/arras-taxonomies.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * arras-taxonomies.php
+ *
+ * Displays settings for Arras tasonomy options
+ *
+ * @author Caspar Green <caspar@iCasparWebDevelopment.com>
+ *
+ * @package Arras
+ * @since 1.6
+ * 
+ */
+
