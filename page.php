9f6554d7ee8ed3972bb9369784c32d0d61daeca0
Caspar Green <caspar@icasparwebdevelopment.com>
2015-04-25 11:17:41 -0400

    Update page.php template for responsive grid

--- page.php
+++ page.php
@@ -1,19 +1,19 @@
 <?php get_header(); ?>
 
-<div id="content" class="section">
+<div id="content" class="<?php echo arras_layout_columns( 'content' ); ?>">
 <?php arras_above_content() ?>
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<?php arras_above_post() ?>
 	<div id="post-<?php the_ID() ?>" <?php arras_single_post_class() ?>>
         <?php arras_postheader() ?>
-        
+
         <div class="entry-content clearfix">
-		<?php the_content( __('<p>Read the rest of this entry &raquo;</p>', 'arras') ); ?>  
-        <?php wp_link_pages(array('before' => __('<p><strong>Pages:</strong> ', 'arras'), 
+		<?php the_content( __('<p>Read the rest of this entry &raquo;</p>', 'arras') ); ?>
+        <?php wp_link_pages(array('before' => __('<p><strong>Pages:</strong> ', 'arras'),
 			'after' => '</p>', 'next_or_number' => 'number')); ?>
 		</div>
-        
+
 		<?php arras_postfooter() ?>
 
         <?php if ( arras_get_option('display_author') ) : ?>
@@ -24,12 +24,12 @@
         </div>
         <?php endif; ?>
     </div>
-    
+
 	<?php arras_below_post() ?>
 	<a name="comments"></a>
     <?php comments_template('', true); ?>
 	<?php arras_below_comments() ?>
-    
+
 <?php endwhile; else: ?>
 
 <?php arras_post_notfound() ?>
