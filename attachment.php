de00b8ebf86079e2aca03b9838573ffaff514643
Caspar Green <caspar@icasparwebdevelopment.com>
2015-04-25 13:47:00 -0400

    Update attachment.php template for responsive grid

--- attachment.php
+++ attachment.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<div id="content" class="section">
+<div id="content" class="<?php echo arras_layout_columns( 'content' ); ?>">
 <?php arras_above_content() ?>
 
 <?php if (have_posts()) : the_post(); ?>
@@ -10,26 +10,26 @@
         <?php arras_postheader() ?>
 
 		<div class="entry-content single-post-attachment clearfix"><?php the_attachment_link($post->ID, false) ?>
-		<?php the_content( __('<p>Read the rest of this entry &raquo;</p>', 'arras') ); ?>	
-        
-        <?php wp_link_pages(array('before' => __('<p><strong>Pages:</strong> ', 'arras'), 
+		<?php the_content( __('<p>Read the rest of this entry &raquo;</p>', 'arras') ); ?>
+
+        <?php wp_link_pages(array('before' => __('<p><strong>Pages:</strong> ', 'arras'),
 			'after' => '</p>', 'next_or_number' => 'number')); ?>
         </div>
-		
+
 		<?php arras_postfooter() ?>
 
-        <?php 
+        <?php
 		if ( arras_get_option('display_author') ) {
 			arras_post_aboutauthor();
 		}
         ?>
     </div>
-    
+
 	<?php arras_below_post() ?>
 	<a name="comments"></a>
     <?php comments_template('', true); ?>
 	<?php arras_below_comments() ?>
-    
+
 <?php else: ?>
 
 <?php arras_post_notfound() ?>
