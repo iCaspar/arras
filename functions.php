eb20eddd05fc1df4906178c02fab07d7b81608a8
Caspar Green <cg@caspar.green>
2017-11-24 14:51:58 -0500

    Change arras_document_title() to title tag support
    Fixes #39

--- functions.php
+++ functions.php
@@ -53,6 +53,7 @@ function arras_setup() {
 		require_once( $locale_file );
 	
 	/* Theme support */
+	add_theme_support( 'title-tag' );
 	add_theme_support('post-thumbnails');
 	add_theme_support('nav-menus');
 	add_theme_support('automatic-feed-links');
