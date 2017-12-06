c7ab779a726ec61102e688bf6eeb79583b603bf7
Caspar Green <cg@caspar.green>
2017-12-06 10:59:21 -0500

    Add AssetService::registerStyles()

--- src/AssetService.php
+++ src/AssetService.php
@@ -12,13 +12,32 @@ namespace Arras;
  */
 class AssetService {
 
+	/**
+	 * @var array
+	 */
 	private $config = [];
 
-	private $env = 'production';
+	/**
+	 * @var string
+	 */
+	private $baseUrl = '';
 
-	public function __construct( array $config ) {
-		$this->config = $config;
-		$this->env    = $this->getEnvironment();
+	/**
+	 * @var bool
+	 */
+	private $isDevEnv = false;
+
+	/**
+	 * AssetService constructor.
+	 *
+	 * @param array $config
+	 * @param string $baseUrl
+	 * @param bool $isDevEnv
+	 */
+	public function __construct( array $config, $baseUrl, $isDevEnv = false ) {
+		$this->config  = $config;
+		$this->baseUrl = $baseUrl;
+		$this->isDevEnv = $isDevEnv;
 	}
 
 	/**
@@ -30,6 +49,54 @@ class AssetService {
 		}
 
 		return 'production';
+	}
+
+	/**
+	 * @throws \RuntimeException
+	 * @return array URLs of registered styles.
+	 */
+	public function registerStyles() {
+		if ( ! $this->hasStyles() ) {
+			throw new \RuntimeException( 'Missing styles in configuration.' );
+		}
+
+		foreach ( $this->config['styles'] as $handle => $args ) {
+			if ( ! isset ( $args['filename'] ) ) {
+				continue;
+			}
+
+			$src      = $this->getAssetUrl( $args['filename'], 'css' );
+			$result[] = wp_register_style(
+				$handle,
+				$src,
+				isset( $args['deps'] ) ? $args['deps'] : [],
+				isset( $args['version'] ) ? $args['version'] : '',
+				isset( $args['media'] ) ? $args['media'] : 'all'
+			) ? $src : '';
+		}
+
+		return $result;
+	}
+
+	/**
+	 * @return bool
+	 */
+	private function hasStyles() {
+		return isset( $this->config['styles'] ) && is_array( $this->config['styles'] );
+	}
+
+	/**
+	 * @param string $filename
+	 * @param string $filetype
+	 *
+	 * @return string The asset url.
+	 */
+	private function getAssetUrl( $filename, $filetype ) {
+		if ( $this->isDevEnv ) {
+			return $this->baseUrl . '/src/' . $filetype . '/' . $filename . '.' . $filetype;
+		} else {
+			return $this->baseUrl . '/dist/' . $filetype . '/' . $filename . '.min.' . $filetype;
+		}
 	}
 
 }
--- tests/AssetServiceTest.php
+++ tests/AssetServiceTest.php
@@ -5,15 +5,66 @@
 
 namespace Arras;
 
-use PHPUnit\Framework\Exception;
 use PHPUnit\Framework\TestCase;
+use Brain\Monkey;
+use Brain\Monkey\Functions;
+
 
 class AssetServiceTest extends TestCase {
 
-	public function test__construct() {
-		try {
-			self::assertInstanceOf( 'Arras\AssetService', new AssetService( [] ) );
-		} catch ( Exception $e ) {
-		}
+	protected function setUp() {
+		parent::setUp();
+		Monkey\setUp();
+	}
+
+	protected function tearDown() {
+		Monkey\tearDown();
+		parent::tearDown();
+	}
+
+	public function test__construct(): void {
+		self::assertInstanceOf( 'Arras\AssetService', new AssetService( [], '' ) );
+	}
+
+	public function testRegisterStylesFailNoConfig(): void {
+		self::expectException( 'RuntimeException' );
+		$service = new AssetService( [], '' );
+
+		$service->registerStyles();
+	}
+
+	public function testRegisterProdStyles(): void {
+		Functions\when( 'wp_register_style' )->justReturn( true );
+		$service = $this->getSampleService();
+
+		$result = $service->registerStyles();
+
+		self::assertEquals( [
+			'https://example.com/wp-content/themes/arras/assets/dist/css/style.min.css',
+		], $result );
+	}
+
+	public function testRegisterDevStyles(): void {
+		Functions\when( 'wp_register_style' )->justReturn( true );
+		$service = $this->getSampleService( true );
+
+		$result = $service->registerStyles();
+
+		self::assertEquals( [
+			'https://example.com/wp-content/themes/arras/assets/src/css/style.css',
+		], $result );
+	}
+
+	private function getSampleService( bool $isDevEnv = false ): AssetService {
+		return new AssetService( [
+			'styles' => [
+				'test-styles' => [
+					'filename' => 'style',
+					'deps'     => [],
+					'version'  => '1.2.3',
+					'media'    => 'all'
+				]
+			],
+		], 'https://example.com/wp-content/themes/arras/assets', $isDevEnv );
 	}
 }
