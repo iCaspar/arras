6af9bdfedc0960be0e10f001a873b4b0028be757
Caspar Green <cg@caspar.green>
2017-12-09 19:51:13 -0500

    Remove old favicon from header

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 <head profile="http://gmpg.org/xfn/11">
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>" charset="<?php bloginfo('charset'); ?>" />
 
@@ -8,7 +9,7 @@
 
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
-<meta name="viewport" content="width=1000" />
+<meta name="viewport" content="width=device-width" />
 
 <?php
 wp_enqueue_script( 'superfish', get_template_directory_uri() . '/js/superfish/superfish.js', array( 'jquery' ), '2011-12-01' );
