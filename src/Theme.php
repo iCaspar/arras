1a3981cc418779fed358322589706ed768634b82
Caspar Green <cg@caspar.green>
2017-12-12 08:40:25 -0500

    Add error check for missing assets in Theme class

--- src/Theme.php
+++ src/Theme.php
@@ -12,21 +12,40 @@ namespace Arras;
  */
 class Theme {
 
+	/**
+	 * @var array
+	 */
 	private $config = [];
 
+	/**
+	 * Theme constructor.
+	 *
+	 * @param array $config
+	 */
 	public function __construct( array $config ) {
 		$this->config = $config;
 	}
 
+	/**
+	 * @return void
+	 */
 	public function init() {
 		if ( isset( $this->config['assets'] ) && is_array( $this->config['assets'] ) ) {
 			add_action( 'wp_enqueue_scripts', [ $this, 'initAssets' ] );
 		}
 	}
 
+	/**
+	 * @return void
+	 */
 	public function initAssets() {
+		if ( ! isset( $this->config['assets'] ) ) {
+			return;
+		}
+
 		$isDevEnv = defined( 'SCRIPT_DEBUG' ) && true == SCRIPT_DEBUG;
 		$assets   = new AssetService( $this->config['assets'], ARRAS_ASSET_URL, $isDevEnv );
+
 		try {
 			$result = $assets->registerStyles();
 		} catch ( \RuntimeException $e ) {
