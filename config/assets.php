d1a0beb5adfa74d83502c17fc5feb8c2bb84f1fe
Caspar Green <cg@caspar.green>
2017-12-12 16:30:39 -0500

    Add gradient, shadows, fix image urls

--- config/assets.php
+++ config/assets.php
@@ -7,34 +7,42 @@ return [
 	'styles' => [
 		'arras'        => [
 			'filename' => 'default',
+			'version'  => ARRAS_VERSION,
 			'scheme'   => true,
 		],
 		'arras-blue'   => [
 			'filename' => 'blue',
+			'version'  => ARRAS_VERSION,
 			'scheme'   => true,
 		],
 		'arras-green'  => [
 			'filename' => 'green',
+			'version'  => ARRAS_VERSION,
 			'scheme'   => true,
 		],
 		'arras-orange' => [
 			'filename' => 'orange',
+			'version'  => ARRAS_VERSION,
 			'scheme'   => true,
 		],
 		'arras-red'    => [
 			'filename' => 'red',
+			'version'  => ARRAS_VERSION,
 			'scheme'   => true,
 		],
 		'arras-violet' => [
 			'filename' => 'violet',
+			'version'  => ARRAS_VERSION,
 			'scheme'   => true,
 		],
 		'arras-legacy' => [
 			'filename' => 'legacy',
+			'version'  => ARRAS_VERSION,
 			'scheme'   => true,
 		],
 		'arras-rtl'    => [
 			'filename' => 'rtl',
+			'version'  => ARRAS_VERSION,
 		],
 	],
 ];
