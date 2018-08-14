4e388a61c8abaeef81b1dfeb2a356c476304ab8d
Caspar Green <cg@caspar.green>
2018-08-14 11:12:44 -0400

    Update footer.php to WPCS

--- footer.php
+++ footer.php
@@ -1,33 +1,50 @@
+<?php
+/**
+ * Arras footer template.
+ *
+ * @package Arras
+ *
+ * @since 1.0.0
+ */
+
+?>
+
+</div>
+<?php arras_before_footer(); ?>
+
+<div id="footer" class="site-footer">
+	<div class="footer-sidebar-container">
+		<?php
+		$arras_footer_sidebars = arras_get_option( 'footer_sidebars' );
+
+		if ( ! $arras_footer_sidebars ) {
+			$arras_footer_sidebars = 1;
+		}
+
+		for ( $arras_footer_sidebar_count = 1; $arras_footer_sidebar_count < $arras_footer_sidebars + 1; $arras_footer_sidebar_count ++ ) :
+			?>
+			<ul id="footer-sidebar-<?php echo esc_attr( $arras_footer_sidebar_count ); ?>" class="footer-sidebar clearfix xoxo">
+				<?php if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'Footer Sidebar #' . $arras_footer_sidebar_count ) ) : ?>
+					<li></li>
+				<?php endif; ?>
+			</ul>
+		<?php endfor; ?>
 	</div>
 
-	<?php arras_before_footer() ?>
-    
-    <div id="footer" class="site-footer">
-		<div class="footer-sidebar-container clearfix">
-			<?php 
-				$footer_sidebars = arras_get_option('footer_sidebars');
-				if ($footer_sidebars == '') $footer_sidebars = 1;
-				
-				for ($i = 1; $i < $footer_sidebars + 1; $i++) : 
-			?>
-				<ul id="footer-sidebar-<?php echo $i ?>" class="footer-sidebar clearfix xoxo">
-					<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer Sidebar #' . $i) ) : ?>
-					<li></li>
-					<?php endif; ?>
-				</ul>
-			<?php endfor; ?>
-		</div>
-		
-		<div class="footer-message">
-		<p class="floatright"><a class="arras" href="<?php echo ARRAS_URL; ?>"><strong><?php _e('About Arras WordPress Theme', 'arras') ?></strong></a></p>
-		<?php echo stripslashes(arras_get_option('footer_message')); ?>		
-		</div><!-- .footer-message -->
-    </div>
-</div><!-- #wrapper -->
-<?php 
+	<div class="footer-message">
+		<p class="floatright"><a class="arras" href="<?php echo esc_url( ARRAS_URL ); ?>">
+				<strong><?php esc_attr_e( 'About Arras WordPress Theme', 'arras' ); ?></strong>
+			</a>
+		</p>
+		<?php echo wp_kses_post( arras_get_option( 'footer_message' ) ); ?>
+	</div>
+</div>
+</div>
+
+<?php
 arras_footer();
-wp_footer(); 
+wp_footer();
 ?>
+
 </body>
 </html>
-   
