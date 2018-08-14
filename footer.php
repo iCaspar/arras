79c3aa1150a9416d50c42ce0116882a3c34da52f
Caspar Green <cg@caspar.green>
2018-08-14 12:01:11 -0400

    Remove hard-coded theme link from footer

--- footer.php
+++ footer.php
@@ -20,10 +20,6 @@ use Arras\Components\Footer;
 	</div>
 
 	<div class="footer-message">
-		<p class="floatright"><a class="arras" href="<?php echo esc_url( ARRAS_URL ); ?>">
-				<strong><?php esc_attr_e( 'About Arras WordPress Theme', 'arras' ); ?></strong>
-			</a>
-		</p>
 		<?php echo wp_kses_post( arras_get_option( 'footer_message' ) ); ?>
 	</div>
 </div>
