2b3fd41d9ac6527e983ebf90ea585440fca279d5
Caspar Green <cg@caspar.green>
2018-08-25 17:32:46 -0400

    Add styles for search template

--- search.php
+++ search.php
@@ -4,7 +4,7 @@
 
 <?php if (have_posts()) : ?>
 <div class="search-results">
-    <h2><?php _e('Search Results', 'arras') ?></h2>
+    <h2 class="results-title"><?php _e('Search Results', 'arras') ?></h2>
     <div class="search-results-content clearfix">
 	<p><?php _e('Search Results for', 'arras'); ?> <strong>&#8216;<?php echo esc_html($s); ?>&#8217;</strong></p>
     <?php get_search_form(); ?>
@@ -16,29 +16,29 @@
 </div>
 
 <?php if(function_exists('wp_pagenavi')) wp_pagenavi(); else { ?>
-    <div class="navigation clearfix">
-		<div class="floatleft"><?php previous_posts_link( __('Newer Entries &raquo;', 'arras') ) ?></div>
-		<div class="floatright"><?php next_posts_link( __('&laquo; Older Entries', 'arras') ) ?></div>
+    <div class="entries-nav navigation clearfix">
+	    <div class="next-posts floatright"><?php next_posts_link( __('&laquo; Older Entries', 'arras') ) ?></div>
+	    <div class="previous-posts floatleft"><?php previous_posts_link( __('Newer Entries &raquo;', 'arras') ) ?></div>
     </div>
 <?php } ?>
 
 <?php else: ?>
 
 <div class="search-results">
-    <h2>Search Results</h2>
+    <h2 class="results-title">Search Results</h2>
     <div class="search-results-content clearfix">
     <p><?php _e('<strong>Sorry, we couldn\'t find any results based on your search query.</strong>', 'arras') ?></p>
     <?php get_search_form() ?>
     </div>
 </div> 
 
-<h2 class="home-title"><?php _e('Blog Archive', 'arras') ?></h2>
+<h2 class="results-title home-title"><?php _e('Blog Archive', 'arras') ?></h2>
 <?php arras_render_posts( null, arras_get_option('archive_display') ) ?>
     
 <?php if(function_exists('wp_pagenavi')) wp_pagenavi(); else { ?>
-    <div class="navigation clearfix">
-		<div class="floatleft"><?php next_posts_link( __('&laquo; Older Entries', 'arras') ) ?></div>
-		<div class="floatright"><?php previous_posts_link( __('Newer Entries &raquo;', 'arras') ) ?></div>
+    <div class="entries-nav navigation clearfix">
+		<div class="next-posts floatleft"><?php next_posts_link( __('&laquo; Older Entries', 'arras') ) ?></div>
+		<div class="previous-posts floatright"><?php previous_posts_link( __('Newer Entries &raquo;', 'arras') ) ?></div>
     </div>
 <?php } ?>  
 <?php endif; ?>
